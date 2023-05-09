455e1d6113f658de65e485537493089b83ed6323
FedeBirge <fedeikky@gmail.com>
2023-05-08 21:01:34 -0300

    ejercicio Spring MVC

--- src/main/java/com/egg/eggNews/repositorios/NoticiaRepositorio.java
+++ src/main/java/com/egg/eggNews/repositorios/NoticiaRepositorio.java
@@ -19,6 +19,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NoticiaRepositorio extends JpaRepository<Noticia, String> {
     
+    
+    
     @Query("SELECT n FROM noticias n WHERE n.titulo = : titulo")
     public Noticia buscarPorTitulo(@Param("titulo")String titutlo);
     
