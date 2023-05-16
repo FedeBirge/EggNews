c3b20e659b52cd5a96eebeda8e646e364e49ec36
FedeBirge <fedeikky@gmail.com>
2023-05-16 10:01:19 -0300

    ejercicio Spring MVC

--- src/main/java/com/egg/eggNews/controladores/NoticiaControlador.java
+++ src/main/java/com/egg/eggNews/controladores/NoticiaControlador.java
@@ -1,14 +1,18 @@
 
 package com.egg.eggNews.controladores;
 
+import com.egg.eggNews.entidades.Noticia;
 import com.egg.eggNews.excepciones.MyException;
 import com.egg.eggNews.servicios.NoticiaService;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +32,23 @@ public class NoticiaControlador {
     @GetMapping("/registrar") //localhost:8080/panel/noticia/registrar
     public String cargar(){       
         return "noti_form.html";
+    }
+    
+    @GetMapping("/mostrar/{titulo}")
+    public String mostrar(@PathVariable("titulo") String titulo, ModelMap modelo) throws MyException {
+           
+        // getOne con titulo
+        try {
+             System.out.println(notiServ.getOne(titulo));
+            modelo.put("noticia", notiServ.getOne(titulo));
+           
+            return "mostrar.html";
+        } catch (Exception ex) {
+             List<Noticia> noticias = notiServ.listarNoticias();
+        Collections.sort(noticias);
+        modelo.addAttribute("noticias", noticias);
+            return "panelAdmin.html";
+        }
     }
     @PostMapping("/registro") //localhost:8080/panel/noticia/registro
     public String registro(@RequestParam String titulo,@RequestParam String cuerpo, ModelMap modelo) {
--- src/main/java/com/egg/eggNews/controladores/PortalControlador.java
+++ src/main/java/com/egg/eggNews/controladores/PortalControlador.java
@@ -1,6 +1,12 @@
 package com.egg.eggNews.controladores;
 
+import com.egg.eggNews.entidades.Noticia;
+import com.egg.eggNews.servicios.NoticiaService;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,9 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
-    
+     @Autowired
+    private NoticiaService notiServ;
     @GetMapping("/")
-    public String index(){
+    public String index(ModelMap modelo){
+        List<Noticia> noticias = notiServ.listarNoticias();
+        Collections.sort(noticias);
+        modelo.addAttribute("noticias", noticias);
         return "index.html";
     }
     
